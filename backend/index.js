da72da6b96dce864fbd0d192d9d9664a980e7478
PDGamerSG <pallab4586das@gmail.com>
2026-03-24 00:54:27 +0530

    Added routes and zod

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
 import { user } from "./db";
-
 const express = require("express");
-const rootRouter = require("./routes/index");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const mainRouter = require("./routes/index");
 
 const app = express();
 
-app.use("/api/v1",rootRouter);
+app.use("/api/v1",mainRouter);
+app.listen(3000);
+//the urls will be like
+// /api/v1/user
+// /api/v1/transaction ....
+
+//api/v1/user/signin
+//api/v1/user/signup
+//api/v1/user/changePassword .....
+
+//api/v1/account/transaction
